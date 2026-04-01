12cea75461c249ff59679b349f4e415a5f4ecc3c
Karenna Merritt <kamerritt@alaska.edu>
2026-03-31 21:48:32 -0800

    finish adding functions, delete old code. working on troubleshooting

--- game.py
+++ game.py
@@ -104,92 +104,46 @@ class Play:
             fire.draw(WIN) # Draw fires on screen
         
         pygame.display.update()
-        
 
-
-
-
-        
-
-# Function to draw items on gameplay screen 
-def draw(player, time_elapsed, fires)
-    WIN.blit(BG, (0, 0))
-
-    time_text = FONT.render(f'Time: {round(time_elapsed)}s', 1, 'white') # Create gameplay clock
-    WIN.blit(time_text, (10, 10))
-
-    WIN.blit(PLAYER_IMAGE, (player.x, player.y)) # Draw avatar on screen
-
-    for fire in fires:
-        WIN.blit(FIRE_IMAGE, (fire.x, fire.y)) # Draw fire
-
-    pygame.display.update() 
-
-def main():
-    run = True 
-
-    # Place avatar on bottom of gameplay screen 
-    player = PLAYER_IMAGE.get_rect()
-    player.x = 200
-    player.bottom = HEIGHT
-
-    # Increase time on gameplay clock as long as run = True
-    clock = pygame.time.Clock()
-    starttime = time.time()
-    time_elapsed = 0
     
-    fire_add_increment = 2000 # Spawn new fires every 2000 milliseconds
-    fire_count = 0 # Start game with zero fires
+    def end_game(self):
+        # If avatar hits fire, game ends after 4 seconds of "You Lost" text is displayed
+        lost_text = FONT.render('You Lost :(', 1, 'white')
+        WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
 
-    fires = []
-    hit = False
+        pygame.display.update()
+        pygame.time.delay(4000)
 
-    while run:
-        fire_count += clock.tick(60)
-        time_elapsed = time.time() - starttime
+    
+    def run_game(self):
+        while self.run:
+            self.fire_count += self.clock.tick(60) # Increase fire count every second of gameplay
 
-        if fire_count > fire_add_increment:
-            for i in range(3):
-                fire_x = random.randint(0, WIDTH - FIRE_WIDTH)
-                fire = pygame.Rect(fire_x, -FIRE_HEIGHT, FIRE_WIDTH, FIRE_HEIGHT)
-                fires.append(fire) # Add fires to a random location on screen
+            # Spawn fires on screen until game ends
+            if self.fire_count > self.fire_add_increment:
+                self.spawn.fires()
+            
+            # End game if user clicks to exit screen
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
 
-                fire_add_increment = max(200, fire_add_increment - 50)
-                fire_count = 0
+            # Allow avatar movement
+            keys = pygame.key.get_pressed()
+            self.player.move(keys)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False # Allow user to exit game
+            self.fire_location() # Handle fires
+
+            # End game if user is hit by fire
+            if self.hit:
+                self.end_game()
                 break
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
-            player.x -= PLAYER_VEL # Moves avatar left with left arrow key press
-
-        elif keys[pygame.K_RIGHT] and player.x + PLAYER_VEL  +player.width <= WIDTH:
-            player.x += PLAYER_VEL # Moves avatar right with right arrow key press 
-
-        for fire in fires[:]:
-            fire.y += FIRE_VEL
-            if fire.y > HEIGHT:
-                fires.remove(fire)
-            elif fire.y + fire.height >= player.y and fire.colliderect(player):
-                fires.remove(fire)  
-                hit = True # End game if avatar is hit by a flame
-                break
-
-        if hit:
-            # Display 'You Lose' text and end game after 4000 milliseconds
-            lost_text = FONT.render('You lost :(', 1, 'white') 
-            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
-            pygame.display.update()
-            pygame.time.delay(4000)
-            break
-
-        draw(player, time_elapsed, fires)
-
-    pygame.quit()
+                
+            self.draw() # Draw all components until game ends
 
 
+        pygame.quit()
+
+# Run game!
 if __name__ == "__main__":
     main()
